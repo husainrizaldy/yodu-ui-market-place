6729dabee7b3b1ee0c15086d6c26945f3abb2d66
husainrizaldy <husain.rizaldy@gmail.com>
2021-06-02 21:46:43 +0700

    add new source

---
+++ assets/js/libs.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+    // toggle menu
+    $('.nav-toggle').click(function(){
+        $('.nav-toggle').toggleClass('active')
+        $('.menu-nav').toggleClass('active')
+    });
+
+    // change nav background color when scroll
+    $(window).scroll(function() {
+        $('.head-navigation').toggleClass('scrolled', $(this).scrollTop() > 620 );
+    });
+
+});
