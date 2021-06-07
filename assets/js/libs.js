3ea28784675aca5a3becf9efef0b16ee87e86a82
husainrizaldy <husain.rizaldy@gmail.com>
2021-06-07 12:43:42 +0700

    footer, toko-views

--- assets/js/libs.js
+++ assets/js/libs.js
@@ -1,31 +1,11 @@
 $(document).ready(function(){
-
     // toggle menu
     $('.nav-toggle').click(function(){
         $('.nav-toggle').toggleClass('active')
         $('.menu-nav').toggleClass('active')
     });
-
     // change nav background color when scroll
     $(window).scroll(function() {
         $('.head-navigation').toggleClass('scrolled', $(this).scrollTop() > 320 );
     });
-
- 
- 	// var widNew = $(window).width();
- 	// if (widNew <= 475) 
- 	// {
- 	// 	$('.textBtn').addClass('dis-none');
- 	// }
- 	// else
- 	// {
- 	// 	var checkClass = $('.textBtn').hasClass('dis-none');
- 	// 	if (checkClass == true) {
- 	// 		$('.textBtn').removeClass('dis-none');
- 	// 	}
- 		
- 	// }
-    
-    
-
 });
